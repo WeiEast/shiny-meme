fa77631c56288d01680f02684adff4ecb8309383
zhanglun <zhanglun1410@gmail.com>
2014-09-01 22:26:11 +0800

    commit

--- src/javascripts/base.js
+++ src/javascripts/base.js
@@ -3,40 +3,44 @@
     var fakeEvent = {
         deltaY: 0
     };
+    var isRunning = false;
 
-    /*function onMouseWheel(event, instance) {
-        if (event.deltaY < 0) {
-            instance.next(instance);
-        } else {
-            instance.prev(instance);
-        }
-    }*/
-    function onMouseWheel(event){
+    function onMouseWheel(event) {
 
     }
-    function getWheelDelta(event){
-        if (event.wheelDelta){
+
+    function getWheelDelta(event) {
+        if (event.wheelDelta) {
             return event.wheelDelta;
-        }else{
+        } else {
             return -event.detail * 40;
         }
     }
-    function addMouseWheelEvent(elem, handler){
+
+    function addMouseWheelEvent(elem, handler) {
         elem.addEventListener('mousewheel', handler, false);
         elem.addEventListener('DOMMouseScroll', handler, false);
     }
 
+    function startMove(event, container) {
+        var deltaY = getWheelDelta(event);
+        console.log(deltaY);
+        if (deltaY < 0) {
+            console.log(container.style.top);
+            container.style.top = parseInt(container.style.top, 10) - 100 + "%";
+        }
+    }
     window.addEventListener("load", function() {
 
         var winH = document.body.clientHeight;
         var scrollTop = document.body.scrollTop;
         var container = document.getElementById("main-sections");
         container.style.top = "0";
-        var timer = null;
-        var isRunning = true;
-        addMouseWheelEvent(container, function(event){
+        addMouseWheelEvent(container, function(event) {
             event.stopPropagation();
-            console.log(getWheelDelta(event));
+            if(!isRunning){
+                startMove(event, container);
+            }
         });
 
     }, false);
