b548fda977f7fc47969ba18d7a9ab69d0bbe620a
zhanglun <zhanglun1410@gmail.com>
2014-10-16 23:15:10 +0800

    try to refactor

--- CrispElite/scripts/common.js
+++ CrispElite/scripts/common.js
@@ -1,9 +1,15 @@
-/**
- *
- * @authors CrispElite (zhanglun1410@gmail.com)
- * @date    2014-03-21 19:45:35
- * @version 1.0
- */
+(function() {
+    var dxg = {};
+    dxg.getScrollTop = function() {
+        var top = document.body.scrollTop || document.documentElement.scrollTop;
+        return top;
+    }
+
+
+    window.dxg = dxg;
+
+})();
+
 //niceScroll
 $(document).ready(function() {
     $("html").niceScroll({
@@ -17,64 +23,43 @@ $(document).ready(function() {
         cursorborder: 0 // 滚动条边框大小
     });
 });
+
 $(document).ready(function() {
-    $WinW = window.innerWidth,
-    $WinH = window.innerHeight;
+    var $WinW = window.innerWidth,
+        $WinH = window.innerHeight;
 
-    var Crisp = {};
 
-    Crisp.getScrollTop = function() {
-        var top = document.body.scrollTop || document.documentElement.scrollTop;
-        return top;
-    }
     //设置背景图片等的大小适合屏幕
-    Crisp.fixSize = function() {
-        $(".header_nav").css({
-            width: $WinW + "px"
-        });
-    };
-    Crisp.fixSize();
-    //背景滚动
-    $(window).scroll(function() {
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        $(".greet").css({
-            marginTop: parseInt(scrollTop * 0.7) + 10 + "px"
-        });
-        $("#area1").css({
-            backgroundPosition: "center " + parseInt(-(scrollTop + 200) * 0.2 + 50) + "px"
-        });
-        $("#area2").css({
-            backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 2) * 0.2 + 50) + "px"
-        });
-        $("#area3").css({
-            backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 3) * 0.2 + 100) + "px"
-        });
-        $("#area4").css({
-            backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 5) * 0.2 + 150) + "px"
-        });
-        $("#area5").css({
-            backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 5) * 0.2) + "px"
-        });
+    $(".header_nav").css({
+        width: $WinW + "px"
     });
+    //背景滚动
+    // $(window).scroll(function() {
+    //     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    //     $(".greet").css({
+    //         marginTop: parseInt(scrollTop * 0.7) + 10 + "px"
+    //     });
+    //     $("#area1").css({
+    //         backgroundPosition: "center " + parseInt(-(scrollTop + 200) * 0.2 + 50) + "px"
+    //     });
+    //     $("#area2").css({
+    //         backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 2) * 0.2 + 50) + "px"
+    //     });
+    //     $("#area3").css({
+    //         backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 3) * 0.2 + 100) + "px"
+    //     });
+    //     $("#area4").css({
+    //         backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 5) * 0.2 + 150) + "px"
+    //     });
+    //     $("#area5").css({
+    //         backgroundPosition: "center " + parseInt(-(scrollTop - 458 * 5) * 0.2) + "px"
+    //     });
+    // });
 
 });
 
 //loading
 $(document).ready(function() {
-
-    // var Sys = {};
-    // var ua = navigator.userAgent.toLowerCase();
-    // if (window.ActiveXObject)
-    //     Sys.ie = ua.match(/msie ([\d.]+)/)[1]
-    // else if (document.getBoxObjectFor)
-    //     Sys.firefox = ua.match(/firefox\/([\d.]+)/)[1]
-    // else if (window.MessageEvent && !document.getBoxObjectFor)
-    //     Sys.chrome = ua.match(/chrome\/([\d.]+)/)[1]
-    // else if (window.opera)
-    //     Sys.opera = ua.match(/opera.([\d.]+)/)[1]
-    // else if (window.openDatabase)
-    //     Sys.safari = ua.match(/version\/([\d.]+)/)[1];
-
 
     $("#welcome .greet a").bind("click", function(e) {
         document.body.scrollTop = $WinH;
@@ -95,16 +80,6 @@ $(document).ready(function() {
 
 
     (function() {
-        // if (!Sys.chrome) {
-        //     $("#loadDisk").hide();
-        //     var wannering = $("<h1 class=\"wannering\">请使Webkit浏览器(*^__^*)</h1>");
-        //     wannering.appendTo($("#loading"));
-        //     wannering.css({
-        //         lineHeight: wannering.css("height")
-        //     });
-        //     return false;
-        // }
-
         var mark = new Image();
         mark.src = "images/back6.jpg?";
         document.body.scrollTop = 0;
@@ -128,3 +103,10 @@ $(document).ready(function() {
     })();
 });
 
+
+
+(function() {
+    function Actions() {
+
+    }
+})();
