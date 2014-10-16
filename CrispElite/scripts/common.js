b24d0c258f45cca94415fb8615fa87faa1e443da
zhanglun <zhanglun1410@gmail.com>
2014-10-16 20:08:35 +0800

    try useing media queries

--- CrispElite/scripts/common.js
+++ CrispElite/scripts/common.js
@@ -62,18 +62,18 @@ $(document).ready(function() {
 //loading
 $(document).ready(function() {
 
-    var Sys = {};
-    var ua = navigator.userAgent.toLowerCase();
-    if (window.ActiveXObject)
-        Sys.ie = ua.match(/msie ([\d.]+)/)[1]
-    else if (document.getBoxObjectFor)
-        Sys.firefox = ua.match(/firefox\/([\d.]+)/)[1]
-    else if (window.MessageEvent && !document.getBoxObjectFor)
-        Sys.chrome = ua.match(/chrome\/([\d.]+)/)[1]
-    else if (window.opera)
-        Sys.opera = ua.match(/opera.([\d.]+)/)[1]
-    else if (window.openDatabase)
-        Sys.safari = ua.match(/version\/([\d.]+)/)[1];
+    // var Sys = {};
+    // var ua = navigator.userAgent.toLowerCase();
+    // if (window.ActiveXObject)
+    //     Sys.ie = ua.match(/msie ([\d.]+)/)[1]
+    // else if (document.getBoxObjectFor)
+    //     Sys.firefox = ua.match(/firefox\/([\d.]+)/)[1]
+    // else if (window.MessageEvent && !document.getBoxObjectFor)
+    //     Sys.chrome = ua.match(/chrome\/([\d.]+)/)[1]
+    // else if (window.opera)
+    //     Sys.opera = ua.match(/opera.([\d.]+)/)[1]
+    // else if (window.openDatabase)
+    //     Sys.safari = ua.match(/version\/([\d.]+)/)[1];
 
 
     $("#welcome .greet a").bind("click", function(e) {
@@ -106,8 +106,7 @@ $(document).ready(function() {
         // }
 
         var mark = new Image();
-        // mark.src = "http://d2c87l0yth4zbw.cloudfront.net/i/home/lifestyle-browse.jpg";
-        mark.src = "images/back6.jpg?" + getRandomStr();
+        mark.src = "images/back6.jpg?";
         document.body.scrollTop = 0;
         mark.onload = function() {
             document.body.scrollTop = 0;
